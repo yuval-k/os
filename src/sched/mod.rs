f7b5be39c7213f209879b591b27e4ee031d45f6d
Kohavi, Yuval <yuval.kohavi@gmail.com>
2016-10-24 11:13:14 +0300

    sched refactor

--- src/sched/mod.rs
+++ src/sched/mod.rs
@@ -13,14 +13,13 @@ type C = super::platform::Context;
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 pub struct ThreadId(pub usize);
 
-const WAKE_NEVER : u64 = 0xFFFFFFFF_FFFFFFFF;
+const WAKE_NEVER: u64 = 0xFFFFFFFF_FFFFFFFF;
 
 struct Thread {
     ctx: C,
     ready: bool,
     id: ThreadId,
-    wake_on: u64,
-     /* TODO:
+    wake_on: u64, /* TODO:
                    *
                    * wake_on: u32,
                    * id: u32,
@@ -31,11 +30,15 @@ struct Thread {
 // TODO: make this Thread and SMP safe.
 // TODO this is the one mega unsafe class, so it needs to take care of it's on safety.
 pub struct Sched {
-    threads: RefCell<Vec<Box<Thread>>>,
+    sched_impl : RefCell<SchedImpl>
+}
+
+struct SchedImpl {
+    threads: Vec<Box<Thread>>,
     idle_thread: Thread,
-    curr_thread_index: Cell<usize>,
-    thread_id_counter: Cell<usize>,
-    time_since_boot_millies: Cell<u64>,
+    curr_thread_index: Option<usize>,
+    thread_id_counter: usize,
+    time_since_boot_millies: u64,
 }
 
 const IDLE_THREAD_ID: ThreadId = ThreadId(0);
@@ -52,15 +55,16 @@ extern "C" fn thread_start(start: *mut Box<FnBox()>) {
 impl Sched {
     pub fn new() -> Sched {
         Sched {
+        sched_impl : RefCell::new(SchedImpl {
             // fake thread as this main thread..
-            threads : RefCell::new(vec![Box::new(
+            threads: vec![Box::new(
                 Thread{
                     ctx : platform::new_thread(::mem::VirtualAddress(0),::mem::VirtualAddress(0),0),
                     ready: true,
                     id : MAIN_THREAD_ID,
                     wake_on: 0,
                 })
-                ]),
+                ],
             idle_thread: Thread{
                 ctx : platform::new_thread(::mem::VirtualAddress(0), 
                     ::mem::VirtualAddress(platform::wait_for_interrupts as usize), 0),
@@ -68,9 +72,10 @@ impl Sched {
                 id : IDLE_THREAD_ID,
                 wake_on: 0,
             },
-            curr_thread_index : Cell::new(0),
-            thread_id_counter : Cell::new(10),
-            time_since_boot_millies : Cell::new(10),
+            curr_thread_index : Some(0),
+            thread_id_counter : 10,
+            time_since_boot_millies : 0,
+        })
         }
     }
 
@@ -88,17 +93,18 @@ impl Sched {
                         start: ::mem::VirtualAddress,
                         arg: usize) {
         // TODO thread safety and SMP Support
-        self.thread_id_counter.set(self.thread_id_counter.get() + 1);
+        let mut simpl = self.sched_impl.borrow_mut();
+        simpl.thread_id_counter +=1;
 
         let t = Box::new(Thread {
             ctx: platform::new_thread(stack, start, arg),
             ready: true,
-            id: ThreadId(self.thread_id_counter.get()),
+            id: ThreadId(simpl.thread_id_counter),
             wake_on: 0,
         });
 
         let ig = platform::intr::no_interrupts();
-        self.threads.borrow_mut().push(t);
+        simpl.threads.push(t);
         // find an eligble thread
         // threads.map()
     }
@@ -106,8 +112,10 @@ impl Sched {
     // no interrupts here..
     pub fn schedule_no_intr(&self, old_ctx: &C) -> C {
         {
-            let cur_th = self.curr_thread_index.get();
-            self.threads.borrow_mut()[cur_th].ctx = *old_ctx;
+            let mut simpl = self.sched_impl.borrow_mut();
+            if let Some(cur_th_i) = simpl.curr_thread_index {
+                simpl.threads[cur_th_i].ctx = *old_ctx;
+            }
         }
         // find an eligble thread
         // threads.map()
@@ -117,8 +125,11 @@ impl Sched {
     fn schedule_new(&self) -> C {
         // find an eligble thread
         // threads.map()
-        let mut cur_th = self.curr_thread_index.get();
-        let num_threads = self.threads.borrow().len();
+        let mut simpl = self.sched_impl.borrow_mut();
+
+        let mut cur_th = if let Some(cur_th_i) = simpl.curr_thread_index { cur_th_i} else { 0 };
+
+        let num_threads = simpl.threads.len();
         for _ in 0..num_threads {
             cur_th += 1;
             // TODO linker with libgcc/compiler_rt so we can have division and mod
@@ -127,32 +138,39 @@ impl Sched {
             }
 
             {
-                let ref mut cur_thread = self.threads.borrow_mut()[cur_th];
-                if (cur_thread.wake_on != WAKE_NEVER) && (cur_thread.wake_on <= self.time_since_boot_millies.get()) {
+                let time_since_boot_millies = simpl.time_since_boot_millies;
+                let cur_thread = &mut simpl.threads[cur_th];
+                if (cur_thread.wake_on != WAKE_NEVER) &&
+                   (cur_thread.wake_on <= time_since_boot_millies) {
                     cur_thread.wake_on = 0;
                     cur_thread.ready = true;
                 }
             }
 
-            let ref cur_thread = self.threads.borrow()[cur_th];
-            if cur_thread.ready {
-                self.curr_thread_index.set(cur_th);
-                let ctx = cur_thread.ctx;
+            let cur_thread_ready = simpl.threads[cur_th].ready;
+            if cur_thread_ready {
+                simpl.curr_thread_index = Some(cur_th);
+                let ctx = simpl.threads[cur_th].ctx;
                 return ctx;
             }
         }
         // no thread is ready.. time to sleep sleep...
         // return to the idle thread.
         // don't wait for interrupts here, as we might already be in an interrupt..
-        self.idle_thread.ctx
+        simpl.curr_thread_index = None;
+        simpl.idle_thread.ctx
     }
 
     pub fn exit_thread(&self) {
         // disable interrupts
         let ig = platform::intr::no_interrupts();
-        // remove the current thread
-        let cur_th = self.curr_thread_index.get();
-        self.threads.borrow_mut().remove(cur_th);
+     
+        {
+            let mut simpl = self.sched_impl.borrow_mut();
+            // remove the current thread
+            let cur_th = simpl.curr_thread_index.unwrap();
+            simpl.threads.remove(cur_th);
+        }
         let new_context = self.schedule_new();
         // tmp ctx.. we don't really gonna use it...
         let mut c = platform::new_thread(::mem::VirtualAddress(0), ::mem::VirtualAddress(0), 0);
@@ -170,18 +188,25 @@ impl Sched {
     }
 
     fn yeild_thread_no_intr(&self) {
-        let new_context: platform::Context;
-        let curr_thread = self.curr_thread_index.get();
+
+        let curr_thread = self.sched_impl.borrow().curr_thread_index;
 
         // TODO: should we add a mutex for smp?
-        new_context = self.schedule_new();
 
-        if curr_thread != self.curr_thread_index.get() {
+        let new_context = self.schedule_new();
+
+        let new_thread = self.sched_impl.borrow().curr_thread_index;
+
+
+        if curr_thread != new_thread {
             // save the context, and go go go
             // pc needs to be after save context
             // use unsafe cell as the we have a context switch.
-            let threads = unsafe { &mut *self.threads.as_ptr() };
-            let ctx = &mut (threads[curr_thread].ctx);
+            let ctx = {
+                let t = &mut self.sched_impl.borrow_mut().threads[curr_thread.unwrap()];
+                unsafe{ &mut *(&mut t.ctx as *mut C) }
+                // make sure borrow ends before call to switch_context. 
+            };
             platform::switch_context(ctx, &new_context);
             // we don't get here :)
         }
@@ -193,13 +218,15 @@ impl Sched {
         self.block_no_intr()
     }
 
-    pub fn sleep(&self, millis : u32) {
+    pub fn sleep(&self, millis: u32) {
         // disable interrupts
         let ig = platform::intr::no_interrupts();
         {
-            let cur_th = self.curr_thread_index.get();
-            let ref mut cur_thread = self.threads.borrow_mut()[cur_th];
-            cur_thread.wake_on = self.time_since_boot_millies.get() + (millis as u64);
+            let mut simpl = self.sched_impl.borrow_mut();
+            let time_since_boot_millies = simpl.time_since_boot_millies;
+            let cur_th = simpl.curr_thread_index.unwrap();
+            let ref mut cur_thread = simpl.threads[cur_th];
+            cur_thread.wake_on = time_since_boot_millies + (millis as u64);
         }
 
         self.block_no_intr()
@@ -208,9 +235,12 @@ impl Sched {
     // assume interrupts are blocked
     pub fn block_no_intr(&self) {
         {
-            let ref mut t = self.threads.borrow_mut()[self.curr_thread_index.get()];
+            let mut simpl = self.sched_impl.borrow_mut();
+            let cur_th = simpl.curr_thread_index.unwrap();
+
+            let ref mut t = simpl.threads[cur_th];
             t.ready = false;
-            if t.wake_on != 0 {
+            if t.wake_on == 0 {
                 t.wake_on = WAKE_NEVER;
             }
         }
@@ -219,7 +249,9 @@ impl Sched {
 
     // assume interrupts are blocked
     pub fn wakeup_no_intr(&self, tid: ThreadId) {
-        for t in self.threads.borrow_mut().iter_mut().filter(|x| x.id == tid) {
+        let mut simpl = self.sched_impl.borrow_mut();
+
+        for t in simpl.threads.iter_mut().filter(|x| x.id == tid) {
             // there can only be one..
             t.wake_on = 0;
             t.ready = true;
@@ -228,7 +260,9 @@ impl Sched {
     }
 
     pub fn get_current_thread(&self) -> ThreadId {
-        return self.threads.borrow()[self.curr_thread_index.get()].id;
+        let simpl = self.sched_impl.borrow();
+
+        return simpl.threads[simpl.curr_thread_index.unwrap()].id;
     }
 
     // TODO
@@ -241,8 +275,10 @@ impl Sched {
 impl platform::InterruptSource for Sched {
     // this method is called platform::ticks_in_second times a second
     fn interrupted(&self, ctx: &mut platform::Context) {
-        let delta_millis = (1000 / platform::ticks_in_second) as u64; 
-        self.time_since_boot_millies.set(self.time_since_boot_millies.get() + delta_millis);
+        const DELTA_MILLIS: u64= (1000 / platform::ticks_in_second) as u64;
+        
+        let mut simpl = self.sched_impl.borrow_mut();
+        simpl.time_since_boot_millies +=  DELTA_MILLIS;
         *ctx = self.schedule_no_intr(ctx);
     }
 }
